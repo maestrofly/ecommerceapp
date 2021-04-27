aea1e5dbe4351981fcca5b606779cf22152798c3
DerrickDarku <derrickalberto@ymail.com>
2021-04-27 07:58:36 +0100

    added products

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,21 +3,22 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } fro
 import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
-// import classes from '*.module.css';
+
 
 const Product = ({ product }) => {
     const classes = useStyles();
 
+
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name} />
+            <CardMedia className={classes.media} image={product.media.source} title={product.name} />
             <CardContent>
               <div className={classes.CardContent}>
                     <Typography variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
                     <Typography variant="h5" >
-                        {product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography>
                 </div> 
                 <Typography variant="body2" color="textSecondary">{product.description}</Typography> 
